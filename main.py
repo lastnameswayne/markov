bf1f57046fada97eb0c20bb2f4c6cc9a80eae07c
lastnameswayne <tiarnan1109@gmail.com>
2022-12-05 17:20:26 +0100

    Ready to hand in

--- main.py
+++ main.py
@@ -1,116 +1,4 @@
-from re import L
-from tkinter import E
 import numpy as np
-
-def init_model():
-    init_probs_43_state = np.zeros(43, dtype = float)
-    init_probs_43_state[0] = 1.0
-
-    trans_probs_43_state = np.zeros((43, 43), dtype = float)
-    trans_probs_43_state[0,  0] = 0.99666   #Staying in the non-coding state
-    trans_probs_43_state[0,  1] = 0.0144    #ATG start codon — Probably too likely
-    trans_probs_43_state[1,  2] = 1.0
-    trans_probs_43_state[2,  3] = 1.0
-    trans_probs_43_state[3,  10] = 1.0
-    trans_probs_43_state[0,  4] = 0.0012    #GTG start codon
-    trans_probs_43_state[4,  5] = 1.0
-    trans_probs_43_state[5,  6] = 1.0
-    trans_probs_43_state[6,  10] = 1.0
-    trans_probs_43_state[0,  7] = 0.0015    #TTG start codon
-    trans_probs_43_state[7,  8] = 1.0
-    trans_probs_43_state[8,  9] = 1.0
-    trans_probs_43_state[9,  10] = 1.0
-    trans_probs_43_state[0, 22] = 0.0166    #TTA reverse start codon
-    trans_probs_43_state[22, 23] = 1.0
-    trans_probs_43_state[23, 24] = 1.0
-    trans_probs_43_state[24, 31] = 1.0
-    trans_probs_43_state[0, 25] = 0.003     #CTA reverse start codon
-    trans_probs_43_state[25, 26] = 1.0
-    trans_probs_43_state[26, 27] = 1.0
-    trans_probs_43_state[27, 31] = 1.0
-    trans_probs_43_state[0, 28] = 0.0017    #TCA reverse start codon
-    trans_probs_43_state[28, 29] = 1.0
-    trans_probs_43_state[29, 30] = 1.0
-    trans_probs_43_state[30, 31] = 1.0
-    trans_probs_43_state[10, 11] = 1.0      #Forward internal codon
-    trans_probs_43_state[11, 12] = 1.0
-    trans_probs_43_state[12, 10] = 0.99886
-    trans_probs_43_state[31, 32] = 1.0      #Reverse internal codon
-    trans_probs_43_state[32, 33] = 1.0
-    trans_probs_43_state[33, 31] = 0.99889
-    trans_probs_43_state[12, 13] = 0.0021      #TAG stop codon
-    trans_probs_43_state[13, 14] = 1.0
-    trans_probs_43_state[14, 15] = 1.0
-    trans_probs_43_state[15, 0] = 1.0
-    trans_probs_43_state[12, 16] = 0.0015      #TGA stop codon
-    trans_probs_43_state[16, 17] = 1.0
-    trans_probs_43_state[17, 18] = 1.0
-    trans_probs_43_state[18, 0] = 1.0
-    trans_probs_43_state[12, 19] = 0.0015      #TAA stop codon
-    trans_probs_43_state[19, 20] = 1.0
-    trans_probs_43_state[20, 21] = 1.0
-    trans_probs_43_state[21, 0] = 1.0
-    trans_probs_43_state[33, 34] = 0.0093      #CAT reverse stop codon
-    trans_probs_43_state[34, 35] = 1.0
-    trans_probs_43_state[35, 36] = 1.0
-    trans_probs_43_state[36, 0] = 1.0
-    trans_probs_43_state[33, 37] = 0.0009      #CAC reverse stop codon
-    trans_probs_43_state[37, 38] = 1.0
-    trans_probs_43_state[38, 39] = 1.0
-    trans_probs_43_state[39, 0] = 1.0
-    trans_probs_43_state[33, 40] = 0.0009      #CAA reverse stop codon
-    trans_probs_43_state[40, 41] = 1.0
-    trans_probs_43_state[41, 42] = 1.0
-    trans_probs_43_state[42, 0] = 1.0
-
-    #ACGT
-    emission_probs_43_state = np.zeros((43, 4), dtype = float)
-    emission_probs_43_state[0] = [0.33434, 0.16479, 0.16613, 0.33474]
-    emission_probs_43_state[1] = [1.0, 0.0, 0.0, 0.0]   #ATG start codon
-    emission_probs_43_state[2] = [0.0, 0.0, 0.0, 1.0]
-    emission_probs_43_state[3] = [0.0, 0.0, 1.0, 0.0]
-    emission_probs_43_state[4] = [0.0, 0.0, 1.0, 0.0]   #GTG start codon
-    emission_probs_43_state[5] = [0.0, 0.0, 0.0, 1.0]
-    emission_probs_43_state[6] = [0.0, 0.0, 1.0, 0.0]
-    emission_probs_43_state[7] = [0.0, 0.0, 0.0, 1.0]   #TTG start codon
-    emission_probs_43_state[8] = [0.0, 0.0, 0.0, 1.0]
-    emission_probs_43_state[9] = [0.0, 0.0, 1.0, 0.0]
-    emission_probs_43_state[10] = [0.32121, 0.15903, 0.32256, 0.19712]  #Forward internal codon
-    emission_probs_43_state[11] = [0.35257, 0.20005, 0.13622, 0.31115]
-    emission_probs_43_state[12] = [0.33922, 0.12998, 0.13122, 0.39958]
-    emission_probs_43_state[13] = [0.0, 0.0, 0.0, 1.0]   #TAG stop codon
-    emission_probs_43_state[14] = [1.0, 0.0, 0.0, 0.0]
-    emission_probs_43_state[15] = [0.0, 0.0, 1.0, 0.0]
-    emission_probs_43_state[16] = [0.0, 0.0, 0.0, 1.0]   #TGA stop codon
-    emission_probs_43_state[17] = [0.0, 0.0, 1.0, 0.0]
-    emission_probs_43_state[18] = [1.0, 0.0, 0.0, 0.0]
-    emission_probs_43_state[19] = [0.0, 0.0, 0.0, 1.0]   #TAA stop codon
-    emission_probs_43_state[20] = [1.0, 0.0, 0.0, 0.0]
-    emission_probs_43_state[21] = [1.0, 0.0, 0.0, 0.0]
-    emission_probs_43_state[22] = [0.0, 0.0, 0.0, 1.0]   #TTA reverse start codon
-    emission_probs_43_state[23] = [0.0, 0.0, 0.0, 1.0]
-    emission_probs_43_state[24] = [1.0, 0.0, 0.0, 0.0]
-    emission_probs_43_state[25] = [0.0, 1.0, 0.0, 0.0]   #CTA reverse start codon
-    emission_probs_43_state[26] = [0.0, 0.0, 0.0, 1.0]
-    emission_probs_43_state[27] = [1.0, 0.0, 0.0, 0.0]
-    emission_probs_43_state[28] = [0.0, 0.0, 0.0, 1.0]   #TCA reverse start codon
-    emission_probs_43_state[29] = [0.0, 1.0, 0.0, 0.0]
-    emission_probs_43_state[30] = [1.0, 0.0, 0.0, 0.0]
-    emission_probs_43_state[31] = [0.39946, 0.13241, 0.12861, 0.33952]  #Reverse internal codon
-    emission_probs_43_state[32] = [0.31271, 0.13711, 0.19756, 0.35261]
-    emission_probs_43_state[33] = [0.19905, 0.31826, 0.16051, 0.32219]
-    emission_probs_43_state[34] = [0.0, 1.0, 0.0, 0.0]   #CAT reverse stop codon
-    emission_probs_43_state[35] = [1.0, 0.0, 0.0, 0.0]
-    emission_probs_43_state[36] = [0.0, 0.0, 0.0, 1.0]
-    emission_probs_43_state[37] = [0.0, 1.0, 0.0, 0.0]   #CAC reverse stop codon
-    emission_probs_43_state[38] = [1.0, 0.0, 0.0, 0.0]
-    emission_probs_43_state[39] = [0.0, 1.0, 0.0, 0.0]
-    emission_probs_43_state[40] = [0.0, 1.0, 0.0, 0.0]   #CAA reverse stop codon
-    emission_probs_43_state[41] = [1.0, 0.0, 0.0, 0.0]
-    emission_probs_43_state[42] = [1.0, 0.0, 0.0, 0.0]
-
-
-    return hmm(init_probs_43_state, trans_probs_43_state, emission_probs_43_state)
 
 def init_7_state():
     init_probs_7_state = np.array([0.00, 0.00, 0.00, 1.00, 0.00, 0.00, 0.00])
@@ -141,7 +29,6 @@ def init_7_state():
 def translate_path_to_indices(path):
     return list(map(lambda x: int(x), path))
 
-
 def translate_annotation_to_hidden_state_7_state(path):
     pathArr = list(path)
     path=pathArr
@@ -170,18 +57,9 @@ def translate_annotation_to_hidden_state_7_state(path):
     return hidden
 
 def translate_indices_to_path_7_state(indices):
-    mapping = np.empty((43), dtype='object')
     mapping = ['C', 'C', 'C', 'N', 'R', 'R', 'R']
     return ''.join([mapping[i] for i in indices])
 
-
-def translate_indices_to_path(indices):
-    mapping = np.empty((43), dtype='object')
-    mapping[0]='N'
-    mapping[1:21]='C'
-    mapping[21:43]='R'
-    #mapping = ['C', 'C', 'C', 'N', 'R', 'R', 'R']
-    return ''.join([mapping[i] for i in indices])
 
 def translate_observations_to_indices(obs):
     mapping = {'a': 0, 'c': 1, 'g': 2, 't': 3}
@@ -217,12 +95,9 @@ def opt_path_prob_log(w):
 
 def backtrack(model, x, w,oldZ):
     x = np.array(x)
-    trans_probs = np.array(model.trans_probs)
-    emission_probs = np.array(model.emission_probs)
     N = w.shape[1]
     z=np.zeros(N, dtype=int)
     z[N-1] = np.argmax(w[:,-1])
-    print("old",oldZ, oldZ.shape)
     for n in range(N-2, -1, -1):
         z[n] = oldZ[int(z[n+1]), n]
     return z
@@ -245,26 +120,6 @@ def compute_w_log(model, x):
             w[j,i] = max(prob)+np.log(emission_probs[j,x[i]])
             z[j,i-1]=np.argmax(prob)
     return w,z
-
-
-def viterbi_log(self, x):
-    K = self.init_probs.shape[0] 
-    # number of states N = Len(x) # length of observation sequence
-    # An indice in the omega matrix k, n represents the probability of being in state k at step n # first column of omega matrix is the start probabilities of the states times their emission probability of the first observation omega = np.ones((K, N)) (np.inf) #k rows n columns
-    N = len(x)
-    omega = np.ones((K,N))*(-np.inf)
-    omega[:,0] = np.log(self.init_probs) + np.log(self.emission_probs[:, x[0]])
-    for n in range(1, N):
-        for k in range(K):
-            omega[k, n] = np.max(
-                omega[:, n - 1] + np.log(self.trans_probs[:, k]) + np.log(self.emission_probs[k, x[n]]))
-    #backtracking
-    Z = np.zeros(N, dtype=int)
-    Z[-1] = np.argmax(omega[:, -1])
-    for n in range (N - 2, -1, -1):
-        Z[n] = np.argmax(
-            np.log(self.emission_probs [Z[n + 1], x[n + 1]])+ omega[:, n] + np.log(self.trans_probs [:, Z[n + 1]]))
-    return Z
 
 def read_fasta_file(filename):
     """
@@ -360,22 +215,18 @@ def train_to_convergence(model, x, z):
     firstModel = training_by_counting(model.init_probs.shape[0], model.emission_probs.shape[1], x,z)
     old_model = firstModel
     iteration = 0
-    #initialTraining
     while True:
-        print("starting iteration", iteration)
+        print("Starting iteration", iteration)
         new_model = viterbi_update_model(old_model, x, model.init_probs.shape[0], model.emission_probs.shape[1])
-
         trans_prob_cmp = abs(new_model.trans_probs - old_model.trans_probs) < 0.01
         emission_prob_cmp = abs(new_model.emission_probs - old_model.emission_probs) < 0.01
         init_prob_cmp = abs(new_model.init_probs - old_model.init_probs) < 0.01
-        print("trans_probs",new_model.trans_probs)
-        print("emit_probs", new_model.emission_probs)
-        if (np.all(trans_prob_cmp) and np.all(emission_prob_cmp) and np.all(init_prob_cmp)) or iteration>100:
-            print("Converged  at iteration " + str(iteration))
+        if (np.all(trans_prob_cmp) and np.all(emission_prob_cmp) and np.all(init_prob_cmp)):
+            print("Finished at iteration " + iteration)
             break
 
         old_model = new_model
-        iteration += 1
+        iteration = iteration + 1
     return new_model
         
 
@@ -386,33 +237,7 @@ def compute_accuracy(true_ann, pred_ann):
                for i in range(len(true_ann))) / len(true_ann) 
 
 
-
-# x1=g1['genome1']
-# trueAnnotations1=(g1_true['true-ann1'])
 myModel = init_7_state()
-print("before trans_probs",myModel.trans_probs)
-print("before emission probs", myModel.emission_probs)
-# Your code to read the annotations and compute the accuracies of your predictions...
-
-
-# g1 = read_fasta_file('genome1.fa')
-# g1_true = read_fasta_file('true-ann1.fa')
-# x=g1['genome1'][:10000]
-# true = g1_true['true-ann1'][:10000]
-# x_indices=translate_observations_to_indices(x)
-# training_by_counting(43,4, x_indices,true, x)
-# w,z = compute_w_log(myModel, x)
-# z_viterbi = backtrack(myModel, x, w, z)
-# print("acc",compute_accuracy(true,translate_indices_to_path(z_viterbi)))
-
-# print("starting training")
-# myTrainedModel = train_to_convergence(myModel, x)
-# w_trained,z_trained = compute_w_log(myTrainedModel, x)
-# z_viterbi_trained = backtrack(myTrainedModel, x, w_trained, z_trained)
-# print("acc",compute_accuracy(true,translate_indices_to_path(z_viterbi_trained)))
-
-# finalS = ''.join(translate_indices_to_path(z_viterbi))
-
 
 
 def trainOnAll5FindBestModel():
@@ -524,18 +349,18 @@ def FiveFoldCrossValidation():
     z_5=translate_annotation_to_hidden_state_7_state(g5_true)
 
     # #ROUND 1, train on 2,3,4,5
-    # myTrainedModel2 = train_to_convergence(myModel, x_2, z_2)
-    # myTrainedModel3 = train_to_convergence(myTrainedModel2, x_3, z_3)
-    # myTrainedModel4 = train_to_convergence(myTrainedModel3, x_4, z_4)
-    # myTrainedModel5 = train_to_convergence(myTrainedModel4, x_5, z_5)
-    # #ROUND 1, predict on 1
-    # w_1,z_w_log_1 = compute_w_log(myTrainedModel5, x_1)
-    # z_viterbi_1 = backtrack(myTrainedModel5, x_1, w_1, z_w_log_1)
-    # finalS = ''.join(translate_indices_to_path_7_state(z_viterbi_1))
-    # with open('predict1.fa', 'w') as fp:
-    #     fp.write(finalS)
+    myTrainedModel2 = train_to_convergence(myModel, x_2, z_2)
+    myTrainedModel3 = train_to_convergence(myTrainedModel2, x_3, z_3)
+    myTrainedModel4 = train_to_convergence(myTrainedModel3, x_4, z_4)
+    myTrainedModel5 = train_to_convergence(myTrainedModel4, x_5, z_5)
+    #ROUND 1, predict on 1
+    w_1,z_w_log_1 = compute_w_log(myTrainedModel5, x_1)
+    z_viterbi_1 = backtrack(myTrainedModel5, x_1, w_1, z_w_log_1)
+    finalS = ''.join(translate_indices_to_path_7_state(z_viterbi_1))
+    with open('predict1.fa', 'w') as fp:
+        fp.write(finalS)
 
-    # print("DONE WITH ROUND 1")
+    print("DONE WITH ROUND 1")
 
     #ROUND 2, train on 1,3,4,5
     print("STARTING ROUND 2")
@@ -591,41 +416,9 @@ def FiveFoldCrossValidation():
     with open('predict5.fa', 'w') as fp:
         fp.write(finalS)
 
-#trainOnAll5FindBestModel()
+print("Starting to find best model and predicting genome 6 through 10")
+trainOnAll5FindBestModel()
+print("Done with predicting genome 6 through 10")
+print("Starting Five Fold Cross Validation")
 FiveFoldCrossValidation()
-#decide on model
-
-
-
-
-# def main(K, D, trueAnnotations, model):
-#     w = compute_w_log(model, trueAnnotations)
-#     z = backtrack_log(model, x, w)
-#     for i in range(50):
-#         final_hmm = viterbi_update_model(final_hmm, x, K, D)
-
-#     return final_hmm
-
-#decide on K and D??
-#find initial probabilities and model
-#initialisere en model?
-
-
-#x is genome1
-#use viterbi decoding to find the most likely hidden states from the model og x
-#brug den viterbi decoding til at train by counting
-#kør training by counting indtil modellens parametre ikke rykker sig længere
-#brug den model til at sammenligne 
-#sammenlign accuracy
-#iterate igen
-
-#når det er færdigt så:
-#5 runder:
-#runde 1: traning by counting på genome 2,3,4,5
-            # predict din gene structure af genome 1
-            #compute the approximate correlation coefficient mellem genome1 og true-genome 1
-
-
-
-#brug din bedste model til at preduct genome 6,7,8,9,10
-
+print("Finished Five Fold Cross Validation")
